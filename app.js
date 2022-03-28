f3ecc38cae728b021ce26b76c09ff5a3fe8036e7
emrahsusam <emrahsusam01@gmail.com>
2022-03-28 14:40:45 +0300

    Changed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const createError       =require('http-errors');
 const mongoose = require("mongoose");
 const path = require("path");
 const fs = require("fs");
@@ -106,6 +107,22 @@ app.use(orderRoute);
 app.use(adminRoute);
 app.use(paymentRoute);
 
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started at port ${process.env.PORT || 3000}`);
